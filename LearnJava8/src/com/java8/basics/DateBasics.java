f0e88dce75129fbbd5bc7abb0a388af567d320a9
Minal Kondawar <mkondawar11@gmail.com>
2017-02-09 23:44:11 -0600

    First commit

---
+++ LearnJava8/src/com/java8/basics/DateBasics.java
@@ -0,0 +1,10 @@
+package com.java8.basics;
+
+public class DateBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Testing");
+	}
+
+}
